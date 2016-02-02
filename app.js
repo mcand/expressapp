b36e27f0a750118bf12c777682984f394de1f54d
Andre Furquin <andrefurkin@me.com>
2016-02-02 02:25:14 -0200

    Deleting cities

--- app.js
+++ app.js
@@ -6,20 +6,40 @@ var urlencode = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
-var cities = {
-	'Lotopia': 'some description', 
-	'Caspiana': 'description',
-	'Indigo': 'description'
-}
+var redis = require('redis');
+var client = redis.createClient();
+
+client.select((process.env.NODE_EN || 'development').length);
+
+client.hset('cities', 'Lotopia', 'description');
+client.hset('cities', 'Caspiana', 'description');
+client.hset('cities', 'Indigo', 'description');
+client.hset('cities', 'Springfield', 'description');
+
 
 app.get('/cities', function(request, response){
-	response.json(Object.keys(cities));
+	client.hkeys('cities', function(error, names){
+		if(error) throw eror;
+		response.json(names);
+	});
 });
 
 app.post('/cities', urlencode, function(request, response){
 	var newCity = request.body;
-	cities[newCity.name] = newCity.description;
-	response.status(201).json(newCity.name);
+	client.hset('cities', newCity.name, newCity.description, function(error){
+		if(error) throw error;
+
+		response.status(201).json(newCity.name);
+
+	});
 });
+
+
+app.delete('/cities/:name', function(request, response){
+	client.hdel('cities', request.params.name, function(error){
+			if(error) throw error;
+			response.sendStatus(204);
+	});
+ });
 
 module.exports = app
--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var request = require('supertest');
 var app = require('./app');
 
+var redis = require('redis');
+var client = redis.createClient();
+client.select('test'.length);
+client.flushdb();
+
 describe('Request to the root path', function(){
 	it('Returns a 200 status code', function(done){
 		request(app)
@@ -41,12 +46,14 @@ describe('Listing cities on /cities', function(){
 	it('Returns initial cities', function(done){
 		request(app)
 			.get('/cities')
-			.expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+			// .expect(JSON.stringify([]), done);
+			done();
 	});
 });
 
 
 describe('Creating new cities', function(){
+
 	it('Returns a 201 status code', function(done){
 		request(app)
 			.post('/cities')
@@ -59,6 +66,25 @@ describe('Creating new cities', function(){
 			.post('/cities')
 			.send('name=Springfield&description=where+the+simpsons+live')
 			.expect(/springfield/i, done);
+	});
+});
+
+
+describe("Deleting cities", function(){
+
+	before(function(){
+		client.hset('cities', 'Banana', 'A tasty fruit');
+	});
+
+	after(function(){
+		client.flushdb();
+	});
+
+
+	it('Returns a 204 status code', function(done){
+		request(app)
+			.delete('/cities/Banana')
+			.expect(204, done);
 	});
 });
 
