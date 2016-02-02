62e658284186b66cd09a880b11c5dc787a7536fd
Andre Furquin <andrefurkin@me.com>
2016-02-02 02:28:58 -0200

    Add validations for new city

--- test.js
+++ test.js
@@ -67,6 +67,13 @@ describe('Creating new cities', function(){
 			.send('name=Springfield&description=where+the+simpsons+live')
 			.expect(/springfield/i, done);
 	});
+
+	it('Validates city name and description', function(done){
+		request(app)
+			.post('/cities')
+			.send('name=&description=')
+			.expect(400, done);
+	});
 });
 
 
