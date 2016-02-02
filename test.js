4afb489cbcd9f665b2ea0409ba9a20ec5beacf3a
Andre Furquin <andrefurkin@me.com>
2016-02-02 00:49:46 -0200

    Listing cities on the index.html

--- test.js
+++ test.js
@@ -13,3 +13,35 @@ describe('Request to the root path', function(){
 	});
 });
 
+describe('Listing cities on /cities', function(){
+	it('returns 200 status code', function(done){
+		request(app)
+			.get('/cities')
+			.expect(200, done);
+	});
+
+	it('Returns a HTML format', function(done) {
+		request(app)
+			.get('/')
+			.expect('Content-Type', 'text/html; charset=UTF-8', done);
+	});
+
+	it('Returns an index file with Cities', function(done){
+		request(app)
+			.get('/')
+			.expect(/cities/i, done)
+	});
+
+	it('Returns JSON format', function(done){
+		request(app)
+			.get('/cities')
+			.expect('Content-type', /json/, done);
+	});
+
+	it('Returns initial cities', function(done){
+		request(app)
+			.get('/cities')
+			.expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done)
+	});
+});
+
